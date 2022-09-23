4f093f1d92dfd49bbb7c38bef90487ba8b4c74dd
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-09-24 01:05:41 +0530

    Recorder Added

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import YouTube from 'react-youtube';
 import VideoDetail from './VideoDetail';
-console.log(VideoDetail);
 
-class Player extends React.Component {
-  render() {
+
+
+class Player extends React.Component{
+  render(){
     const opts = {
-      height: '390',
-      width: '640',
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-      },
-    };
-
-    return <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />;
-  }
-
-  _onReady(event) {
-    // access to player in all event handlers via event.target
-    event.target.pauseVideo();
+            height: '390',
+            width: '640',
+            playerVars: {
+              // https://developers.google.com/youtube/player_parameters
+              autoplay: 1,
+            },
+          };
+    return <><h1>hello bro</h1></>
   }
 }
+
+// class Player extends React.Component {
+//   render() {
+//     const opts = {
+//       height: '390',
+//       width: '640',
+//       playerVars: {
+//         // https://developers.google.com/youtube/player_parameters
+//         autoplay: 1,
+//       },
+//     };
+
+//     return <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />;
+//   }
+
+//   _onReady(event) {
+//     // access to player in all event handlers via event.target
+//     event.target.pauseVideo();
+//   }
+// }
 
 export default Player;
