14422141ab390abe1690160abacf0a7d25fa9791
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-10-02 19:24:40 +0530

    Tried to Add MergeBlob Functionality

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -2,6 +2,7 @@ import { useReactMediaRecorder } from "react-media-recorder";
 import React, { useEffect, useState } from "react";
 import { Button } from 'rsuite';
 
+
 const RecordView = (props) => {
   const [second, setSecond] = useState("00");
   const [minute, setMinute] = useState("00");
@@ -58,28 +59,87 @@ const RecordView = (props) => {
   // console.log("Audio", mediaBlobUrl);
 
   const updateAudios = () => {
+    if(mediaBlobUrl === undefined) return
+
     setAudios(audios => [...audios , mediaBlobUrl]);
     console.log(audios);
   }
 
-  /////////////////////////////////////////////////////
-
-  // const setMyAudios = () => {
-  //   let myAudios = [];
-  //   myAudios.push(mediaBlobUrl);
-  //   console.log(myAudios);
-  //   setAudios([mediaBlobUrl]);
-  //   // audios.push(mediaBlobUrl);
-  // }
-  // console.log(audios);
-
-  ////////////////////////////////////////////////////
 
   const AudioList = () => {
     const renderedAudios = audios.map((audio , index)=> {
       return <audio src= {audios[index]} controls></audio>
     });
     return <div>{renderedAudios}</div>
+  }
+
+  const mergeAudio = () => {
+    ConcatenateBlobs(audios, "audio/wav", showonscreen);
+  }
+
+
+  function showonscreen(bb) {
+    console.log(bb);
+    // var myurl = URL.createObjectURL(bb);
+    // var par = document.querySelector('.paragraph');
+    // par.src = myurl;
+  }
+
+  function ConcatenateBlobs(blobs, type, callback) {
+    var buffers = [];
+  
+    var index = 0;
+  
+    function readAsArrayBuffer() {
+      if (!blobs[index]) {
+        return concatenateBuffers();
+      }
+      var reader = new FileReader();
+      reader.onload = function(event) {
+        buffers.push(event.target.result);
+        index++;
+        readAsArrayBuffer();
+      };
+      reader.readAsArrayBuffer(blobs[index]);
+    }
+  
+    readAsArrayBuffer();
+  
+  
+    function audioLengthTo32Bit(n) {
+      n = Math.floor(n);
+      var b1 = n & 255;
+      var b2 = (n >> 8) & 255;
+      var b3 = (n >> 16) & 255;
+      var b4 = (n >> 24) & 255;
+     
+      return [b1, b2, b3, b4];
+    }
+    function concatenateBuffers() {
+      var byteLength = 0;
+      buffers.forEach(function(buffer) {
+        byteLength += buffer.byteLength;
+      });
+  
+      var tmp = new Uint8Array(byteLength);
+      var lastOffset = 0;
+      var newData;
+      buffers.forEach(function(buffer) {
+        if (type==='audio/wav' && lastOffset >  0) newData = new Uint8Array(buffer, 44);
+        else newData = new Uint8Array(buffer);
+        tmp.set(newData, lastOffset);
+        lastOffset += newData.length;
+      });
+      if (type==='audio/wav') {
+        tmp.set(audioLengthTo32Bit(lastOffset - 8), 4);
+        tmp.set(audioLengthTo32Bit(lastOffset - 44), 40); // update audio length in the header
+      }
+      var blob = new Blob([tmp.buffer], {
+        type: type
+      });
+      callback(blob);        
+      
+    }
   }
 
   return (
@@ -113,7 +173,8 @@ const RecordView = (props) => {
               <Button onClick={() => { pauseRecording(); stopRecording(); setIsActive(!isActive); updateAudios();}}>
                 Stop
               </Button>
-
+              <br />
+              <Button onClick={() => {mergeAudio()}}>Merge</Button>
         </div>
         <b></b>
       </div>
