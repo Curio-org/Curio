7d6e942ec615ad456261519479b9ef16db7b34a9
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-10-03 17:24:16 +0530

    Clean Code With Error

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -122,92 +122,19 @@ const RecordView = (props) => {
 
         callback(blob);
     }
-}
+  }
 
-if(typeof modules !== 'undefined') {
-    module.export = ConcatenateBlobs;
-}
+  if(typeof modules !== 'undefined') {
+      module.export = ConcatenateBlobs;
+  }
 
-if(typeof window !== 'undefined') {
-    window.ConcatenateBlobs = ConcatenateBlobs;
-}
-
-  ///////////////////////////////////////////////////////////////
-  // var ConcatenateBlobs = require('concatenateblobs');
-
-  // ConcatenateBlobs = (audios , 'audio/wav' , showonscreen(result)) => {
-
-  // }
-
-  // const mergeAudio = () => {
-  //   ConcatenateBlobs(audios, "audio/wav", showonscreen);
-  // }
-
+  if(typeof window !== 'undefined') {
+      window.ConcatenateBlobs = ConcatenateBlobs;
+  }
 
   function showonscreen(bb) {
     console.log(bb);
-    // var myurl = URL.createObjectURL(bb);
-    // var par = document.querySelector('.paragraph');
-    // par.src = myurl;
   }
-
-  // function ConcatenateBlobs(blobs, type, callback) {
-  //   var buffers = [];
-  
-  //   var index = 2;
-  
-  //   function readAsArrayBuffer() {
-  //     if (!blobs[index]) {
-  //       return concatenateBuffers();
-  //     }
-  //     var reader = new FileReader();
-  //     reader.onload = function(event) {
-  //       buffers.push(event.target.result);
-  //       index++;
-  //       readAsArrayBuffer();
-  //     };
-  //     reader.readAsArrayBuffer(blobs[index]);
-  //   }
-  
-  //   readAsArrayBuffer();
-  
-  
-  //   function audioLengthTo32Bit(n) {
-  //     n = Math.floor(n);
-  //     var b1 = n & 255;
-  //     var b2 = (n >> 8) & 255;
-  //     var b3 = (n >> 16) & 255;
-  //     var b4 = (n >> 24) & 255;
-     
-  //     return [b1, b2, b3, b4];
-  //   }
-  //   function concatenateBuffers() {
-  //     var byteLength = 0;
-  //     buffers.forEach(function(buffer) {
-  //       byteLength += buffer.byteLength;
-  //     });
-  
-  //     var tmp = new Uint8Array(byteLength);
-  //     var lastOffset = 0;
-  //     var newData;
-  //     buffers.forEach(function(buffer) {
-  //       if (type==='audio/wav' && lastOffset >  0) newData = new Uint8Array(buffer, 44);
-  //       else newData = new Uint8Array(buffer);
-  //       tmp.set(newData, lastOffset);
-  //       lastOffset += newData.length;
-  //     });
-  //     if (type==='audio/wav') {
-  //       tmp.set(audioLengthTo32Bit(lastOffset - 8), 4);
-  //       tmp.set(audioLengthTo32Bit(lastOffset - 44), 40); // update audio length in the header
-  //     }
-  //     var blob = new Blob([tmp.buffer], {
-  //       type: type
-  //     });
-  //     callback(blob);        
-      
-  //   }
-  // }
-
   return (
     <div>
         <h4>
