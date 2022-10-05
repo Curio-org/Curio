23bc21370762eef17308111969a50fa7d6ca9e47
arjxn.py <arjxn.py@gmail.com>
2022-10-05 17:55:55 +0530

    AddComment Added

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {CommentsHeader} from "./CommentsHeader";
+import { AddComment } from './AddComment';
 
 export class Comments extends React.Component {
   render() {
     return(
       <div>
         <CommentsHeader amountComments={this.props.amountComments}/>
+        <AddComment />
       </div>
     );
   }
