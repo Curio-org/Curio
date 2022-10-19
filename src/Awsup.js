4597500316e9c9ff2037878b28df8ee9a46b8cd2
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-10-20 02:50:38 +0530

    A fileupload function added

---
+++ src/Awsup.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+
+class Awsup extends Component {
+    state = {
+        selectedFile : null,
+        fileUploadedSuccessfully: false
+    }
+
+    onFileChange = event => {
+        this.setState({selectedFile: event.target.files[0]})
+    }
+
+    onFileUpload = () => {
+        const formData = new FormData();
+        formData.append(
+            "demo file",
+            this.state.selectedFile,
+            this.state.selectedFile.name
+        )
+        // will have to call API here
+        console.log(formData);
+        this.setState({selectedFile : null})
+        this.setState({fileUploadedSuccessfully: true})
+    }
+
+    fileData = () => {
+        if(this.state.selectedFile){
+            return(
+            <div>
+                <h2>File Details</h2>
+                <p>File Name : {this.state.selectedFile.name}</p>
+                <p>File Type : {this.state.selectedFile.type}</p>
+                <p>Last Modified : {" "}
+                    {this.state.selectedFile.lastModifiedDate.toDateString()}
+                </p>
+            </div>
+            );
+        } else if (this.state.fileUploadedSuccessfully){
+            return(
+                <div>
+                    <h4>Your File Has Been Uploaded Successfully.</h4>
+                </div>
+            );
+        } else {
+            return(
+                <div>
+                <br />
+                    <h4>Choose A File And Press Upload Button</h4>
+                </div>
+            );
+        }
+    }
+
+    render(){
+        return(
+        <>
+            <div>
+                <h2>Audio Uploader</h2>
+                <div>
+                    <input type='file' onChange={this.onFileChange} />
+                    <button onClick={this.onFileUpload}>Upload</button>
+                </div>
+                {this.fileData()}
+            </div>
+        </>
+        )
+    }
+
+}
+
+export default Awsup;
