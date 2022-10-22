f8f1fefa3adada40f2e46a699871944c8c8292d8
arjxn.py <arjxn.py@gmail.com>
2022-10-22 18:12:15 +0530

    S3 Successfully Added

--- src/components/Recorder/Dropzone.js
+++ src/components/Recorder/Dropzone.js
@@ -1,8 +1,28 @@
 import 'react-dropzone-uploader/dist/styles.css'
 import Dropzone from 'react-dropzone-uploader'
+import axios from 'axios'
 import React from 'react'
 
 const DropUpload = () => {
+
+  const API_ENDPOINT = "https://chfxvqkfj7.execute-api.ap-south-1.amazonaws.com/default/"
+  const handleSubmit =async (files) => {
+    const f = files[0]
+    console.log(f["file"]);
+    // * GET request: presigned URL
+    const response = await axios({
+      method: 'GET',
+      url: API_ENDPOINT
+    })
+    console.log('Response: ', response)
+    // * PUT request: upload file to S3
+    const result = await fetch(response.data.uploadURL, {
+      method: 'PUT',
+      body:f["file"]
+    })
+    console.log('Result: ', result)
+
+  }
 
   const handleChangeStatus = ({ meta, remove }, status) => {
     console.log(status , meta);
@@ -12,6 +32,7 @@ const DropUpload = () => {
     <>
       <div id="toast">Upload</div>
       <Dropzone
+        onSubmit={handleSubmit}
         onChangeStatus={handleChangeStatus}
         maxFiles={1}
         multiple={false}
