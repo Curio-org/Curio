9b42ea4ed21c71faacac00788290815d6b11b146
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-10-29 20:39:04 +0530

    getting audio data as src

--- src/components/Video/translatedAudio.js
+++ src/components/Video/translatedAudio.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
+import { Button } from 'rsuite';
 // import { useParams } from 'react-router-dom';
 
-const TranslatedAudio = async () => {
+const TranslatedAudio = () => {
+    const [audio , setAudio] = useState();
 
-    const API_ENDPOINT = `https://mn80j9wred.execute-api.ap-south-1.amazonaws.com/v1/be-curio?file=avc`;
-    const response = await axios({
-        method: 'GET',
-        url: API_ENDPOINT,
-    })
-    console.log('Response: ', response)
+
+    const gts = async () => {
+        const API_ENDPOINT = `https://mn80j9wred.execute-api.ap-south-1.amazonaws.com/v1/be-curio?file=avc`;
+        const response = await axios({
+            method: 'GET',
+            url: API_ENDPOINT,
+        })
+        console.log('Response: ', response)
+        setAudio(response.data)
+        console.log(audio);
+        return response
+    }
+
+
+    // function sA () {
+    //     console.log();
+    // }
 
 
 
     // let response = axios.get(`https://mn80j9wred.execute-api.ap-south-1.amazonaws.com/v2/be-curio?file=avc`)
 
   return (
-    <audio src={response.data} controls></audio>
+    <>
+      <Button onClick={gts}>Set Audio</Button>
+      <audio src={audio} controls></audio>
+    </>
   )
 }
 
