6580b7f2632cec4fd14de74bfbb8522a6f37acd1
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-11-05 19:39:26 +0530

    Footer Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import './Footer.css'
+import curio from'../assets/images/curio.png'
 
 const Footer = () => {
   return (
     <>
-    <h1>Footer</h1>
+      <div className="curio_footer">
+        <div className="curio_footer-heading">
+          <h1 className="gradient__text">Do you want a video to be Translated?</h1>
+        </div>
+
+        <div className="curio_footer-btn">
+          <p>Request Translation</p>
+        </div>
+
+        <div className="curio_footer_links">
+          <div className="curio_footer_links_logo">
+            <img src={curio} alt="gpt3_logo" />
+            <p>Cluster Innovation Centre, <br/>University Of Delhi. <br/> All Rights Reserved</p>
+          </div>
+          <div className="curio_footer_links_div">
+            <h4>Links</h4>
+            <p>Overall</p>
+            <p>Social Media</p>
+            <p>Counters</p>
+            <p>Contact</p>
+          </div>
+          <div className="curio_footer_links_div">
+            <h4>Company</h4>
+            <p>Terms & Conditions </p>
+            <p>Privacy Policy</p>
+            <p>Contact</p>
+          </div>
+          <div className="curio_footer_links_div">
+            <h4>Get in touch</h4>
+            <p>A Street, B-Town, New Delhi</p>
+            <p>012-132567</p>
+            <p>info@curio.net</p>
+          </div>
+        </div>
+
+        <div className="curio_footer_copyright">
+          <p>@2022 CURIO. All rights reserved.</p>
+        </div>
+      </div>
     </>
   )
 }
