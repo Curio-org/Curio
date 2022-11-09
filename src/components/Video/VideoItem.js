e687e8180e96d9b50130f3b92553c7e17c2dadfd
itAmircic <arjxn.py@gmail.com>
2022-11-10 03:52:43 +0530

    channel name show

--- src/components/Video/VideoItem.js
+++ src/components/Video/VideoItem.js
@@ -8,6 +8,7 @@ const VideoItem = ({video , setVidId , setRecId }) => {
             <img  src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
             <div>
                 <div className='title'><h4>{video.snippet.title}</h4></div>
+                <div className='channel_name'><p>{video.snippet.channelTitle}</p></div>
                 <span className='watch'><Button onClick={() => {setVidId(video.id.videoId)}}>Watch</Button></span>
                 <span className='translate'><Button onClick={() => {setRecId(video.id.videoId)}}>Translate</Button></span>
             </div>
