91cb096a1df6457f9bfd5a4d064aa0cccc2f6a3f
itAmircic <arjxn.py@gmail.com>
2022-11-13 15:39:56 +0530

    twilio API enabled

--- src/components/Footer/Twilio.js
+++ src/components/Footer/Twilio.js
@@ -5,11 +5,33 @@ const Twilio = () => {
     const [number, setNumber] = useState("");
     const [body, setBody] = useState("");
 
+    const onSubmit = async (e) => {
+        await e.preventDefault();
+    
+        const res = await fetch("/api/sendMessage", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ to: number, body: body }),
+        });
+    
+        const data = await res.json();
+    
+        if (data.success) {
+          await setNumber("");
+          await setBody("");
+        } else {
+          await setNumber("An Error has occurred.");
+          await setBody("An Error has occurred.");
+        }
+      };
+
   return (
     <div className="twilio">
     <Container>
     <h2>Send SMS</h2>
-    <Form>
+    <Form onSubmit={onSubmit}>
       <Form.Group>
         <Form.Label htmlFor="to">To</Form.Label>
         <Form.Control value={number} onChange={(e) => setNumber(e.target.value)} />
