b161f8f0f371870abf319e5e28955d62e6f76afe
arjxn-py <arjxn.py@gmail.com>
2022-11-21 02:52:00 +0530

    Added Feature Component

---
+++ src/components/UnderHeader/Feature.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './feature.css';
+
+const Feature = ({ title, text }) => (
+  <div className="curio_feature">
+    <div className="curio_feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="curiofeature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
+
+export default Feature;
