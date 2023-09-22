45bc60ec9ca797b90b7fa769fba18b9a6546760a
arjxn.py <arjxn.py@gmail.com>
2023-09-22 11:33:59 +0530

    Fix Awsup component

--- src/Awsup.tsx
+++ src/Awsup.tsx
@@ -1,74 +1,78 @@
-import React, {Component} from 'react';
+import React, { Component, ChangeEvent } from 'react';
 import axios from 'axios';
 
 class Awsup extends Component {
     state = {
-        selectedFile : null,
-        fileUploadedSuccessfully: false
+        selectedFile: null as File | null,
+        fileUploadedSuccessfully: false,
     }
 
-    onFileChange = event => {
-        this.setState({selectedFile: event.target.files[0]})
+    onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            this.setState({ selectedFile: file });
+        }
     }
 
     onFileUpload = () => {
-        const formData = new FormData();
-        formData.append(
-            "demo file",
-            this.state.selectedFile,
-            this.state.selectedFile.name
-        )
-        // will have to call API here
-        axios.post("https://vcor7lap28.execute-api.ap-south-1.amazonaws.com/prod/audio-upload", formData).then(()=>{
-            this.setState({selectedFile : null})
-            this.setState({fileUploadedSuccessfully: true})
-        })
+        const { selectedFile } = this.state;
 
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append(
+                "demo file",
+                selectedFile,
+                selectedFile.name
+            );
+
+            axios.post("https://vcor7lap28.execute-api.ap-south-1.amazonaws.com/prod/audio-upload", formData).then(() => {
+                this.setState({ selectedFile: null, fileUploadedSuccessfully: true });
+            });
+        }
     }
 
     fileData = () => {
-        if(this.state.selectedFile){
-            return(
-            <div>
-                <h2>File Details</h2>
-                <p>File Name : {this.state.selectedFile.name}</p>
-                <p>File Type : {this.state.selectedFile.type}</p>
-                <p>Last Modified : {" "}
-                    {this.state.selectedFile.lastModifiedDate.toDateString()}
-                </p>
-            </div>
+        const { selectedFile, fileUploadedSuccessfully } = this.state;
+
+        if (selectedFile) {
+            return (
+                <div>
+                    <h2>File Details</h2>
+                    <p>File Name : {selectedFile.name}</p>
+                    <p>File Type : {selectedFile.type}</p>
+                    <p>Last Modified : {selectedFile.lastModified ? new Date(selectedFile.lastModified).toDateString() : 'N/A'}</p>
+                </div>
             );
-        } else if (this.state.fileUploadedSuccessfully){
-            return(
+        } else if (fileUploadedSuccessfully) {
+            return (
                 <div>
                     <h4>Your File Has Been Uploaded Successfully.</h4>
                 </div>
             );
         } else {
-            return(
+            return (
                 <div>
-                <br />
+                    <br />
                     <h4>Choose A File And Press Upload Button</h4>
                 </div>
             );
         }
     }
 
-    render(){
-        return(
-        <>
-            <div>
-                <h2>Audio Uploader</h2>
+    render() {
+        return (
+            <>
                 <div>
-                    <input type='file' onChange={this.onFileChange} />
-                    <button onClick={this.onFileUpload}>Upload</button>
+                    <h2>Audio Uploader</h2>
+                    <div>
+                        <input type='file' onChange={this.onFileChange} />
+                        <button onClick={this.onFileUpload}>Upload</button>
+                    </div>
+                    {this.fileData()}
                 </div>
-                {this.fileData()}
-            </div>
-        </>
-        )
+            </>
+        );
     }
-
 }
 
 export default Awsup;
