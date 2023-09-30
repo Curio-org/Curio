dea44d6eda8480158d1668747b85bfd03cbeb5fd
arjxn.py <arjxn.py@gmail.com>
2023-09-30 18:21:09 +0530

    Fix player component

--- src/components/Video/Player.tsx
+++ src/components/Video/Player.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import YouTube from 'react-youtube';
 import { useParams } from 'react-router-dom';
 import TranslatedAudio from './translatedAudio';
-import './video.css'
-// import { Comments } from '.././comments/Comments';
+import './video.css';
 
-const Player = (props) => {
-  const { vidId } = useParams();
+const Player = (props: any) => {
+  const { vidId } = useParams<{ vidId: string }>();
+
   const opts = {
     height: '390',
     width: '640',
@@ -15,52 +15,64 @@ const Player = (props) => {
     },
   };
 
-  const _onReady = (event) => {
+  const _onReady = (event: any) => {
     event.target.pauseVideo();
-    // event.target.player.mute()
+  };
 
-  }
+  const stateChange = (e: any) => {
+    let yt = e.target as any;
+    let mAudio = document.getElementById('translatedAudio') as HTMLAudioElement | null;
 
-  const stateChange = (e) => {
-    let yt = e.target
-    let mAudio = document.getElementById('translatedAudio')
-    if (mAudio) {
-      mAudio.currentTime = yt.getCurrentTime()
+    if (mAudio && yt.getCurrentTime) {
+      mAudio.currentTime = yt.getCurrentTime() as number;
     }
-  }
+  };
 
-  const playvid = (e) => {
-    let yt = e.target
-    let mAudio = document.getElementById('translatedAudio')
-    if (mAudio) {
-      mAudio.load()
-      mAudio.currentTime = yt.getCurrentTime()
-      mAudio.play()
+  const playvid = (e: any) => {
+    let yt = e.target as any;
+    let mAudio = document.getElementById('translatedAudio') as HTMLAudioElement | null;
+
+    if (mAudio && yt.getCurrentTime) {
+      mAudio.load();
+      mAudio.currentTime = yt.getCurrentTime() as number;
+      mAudio.play();
     }
-  }
+  };
 
   const stopvid = () => {
-    let mAudio = document.getElementById('translatedAudio')
+    let mAudio = document.getElementById('translatedAudio') as HTMLAudioElement | null;
+
     if (mAudio) {
-      mAudio.pause()
+      mAudio.pause();
     }
-  }
+  };
 
-  const playbackchange = (e) => {
-    let yt = e.target
-    let mAudio = document.getElementById('translatedAudio')
-    if(mAudio)
-    mAudio.playbackRate = yt.getPlaybackRate()
-  }
+  const playbackchange = (e: any) => {
+    let yt = e.target as any;
+    let mAudio = document.getElementById('translatedAudio') as HTMLAudioElement | null;
 
-  return <>
-        <div className='youtube'>
-          <YouTube onPlaybackRateChange={playbackchange} videoId={vidId} onPlay={playvid} onPause={stopvid} onEnd={stopvid} opts={opts} onStateChange={stateChange} onReady={_onReady} />
-          <TranslatedAudio/>
-        </div> 
-          {/* <Comments /> */}
-        </>
+    if (mAudio && yt.getPlaybackRate) {
+      mAudio.playbackRate = yt.getPlaybackRate() as number;
+    }
+  };
 
+  return (
+    <>
+      <div className="youtube">
+        <YouTube
+          onPlaybackRateChange={playbackchange}
+          videoId={vidId}
+          onPlay={playvid}
+          onPause={stopvid}
+          onEnd={stopvid}
+          opts={opts}
+          onStateChange={stateChange}
+          onReady={_onReady}
+        />
+        <TranslatedAudio />
+      </div>
+    </>
+  );
+};
 
-}
 export default Player;
