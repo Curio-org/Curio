9d12a50c579e7648a8d72da6c09fcb0eba8b362e
arjxn.py <arjxn.py@gmail.com>
2023-09-30 18:48:57 +0530

    Fix videolist component

--- src/components/Video/VideoList.tsx
+++ src/components/Video/VideoList.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({videos , setVidId , setRecId}) => {
-    const renderedVideos =  videos.map((video, index) => {
-        return <div key={index}>
-                <VideoItem video={video} setVidId={setVidId} setRecId={setRecId}/>
-        </div>
-    });
+interface VideoListProps {
+  videos: Array<any>;
+  setVidId: (videoId: string) => void;
+  setRecId: (videoId: string) => void;
+}
 
-    return <div>{renderedVideos}</div>;
+const VideoList: React.FC<VideoListProps> = ({ videos, setVidId, setRecId }) => {
+  const renderedVideos = videos.map((video, index) => {
+    return (
+      <div key={index}>
+        <VideoItem video={video} setVidId={setVidId} setRecId={setRecId} />
+      </div>
+    );
+  });
+
+  return <div>{renderedVideos}</div>;
 };
+
 export default VideoList;
