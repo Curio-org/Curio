dd29cb2cda011fdc954779d6c61c9af359ddb685
arjxn.py <arjxn.py@gmail.com>
2023-10-13 12:10:47 +0530

    Format updated files with prettier

--- src/components/UnderHeader/APIError.tsx
+++ src/components/UnderHeader/APIError.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Message } from 'semantic-ui-react';
-import './APIError.css';
+import React from "react";
+import { Message } from "semantic-ui-react";
+import "./APIError.css";
 
 interface APIErrorProps {
   apiResponse: string;
@@ -10,7 +10,7 @@ class APIError extends React.Component<APIErrorProps> {
   render() {
     return (
       <div className="apiErrorMessage_mainDiv">
-        {this.props.apiResponse !== '' && (
+        {this.props.apiResponse !== "" && (
           <Message className="apiErrorMessage__div">
             <Message className="apiErrorMessage__header">API Error</Message>
             <p>{this.props.apiResponse}</p>
