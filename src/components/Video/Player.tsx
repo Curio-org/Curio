fbd5faa4e8efa57d7c1f8e820cae0cb104d1905c
Omar Hussein <omarmohammad1951@gmail.com>
2023-10-20 12:25:51 -0400

    added responsive view for video playback page

--- src/components/Video/Player.tsx
+++ src/components/Video/Player.tsx
@@ -65,7 +65,6 @@ const Player = (props: any) => {
   };
 
   return (
-    <>
       <div className="youtube">
         <YouTube
           onPlaybackRateChange={playbackchange}
@@ -79,7 +78,6 @@ const Player = (props: any) => {
         />
         <TranslatedAudio />
       </div>
-    </>
   );
 };
 
