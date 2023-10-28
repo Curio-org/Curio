34d6db382335dd71b153fee61cfa323c138b1a9f
arjxn.py <arjxn.py@gmail.com>
2023-10-28 13:45:55 +0530

    Added social links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 // import { Header, Image } from 'semantic-ui-react'
-import Curiologo from '../assets/images/Csmall.png'
-import './header.css';
-import { Link ,BrowserRouter as Router } from 'react-router-dom';
-import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import Curiologo from "../assets/images/Csmall.png";
+import "./header.css";
+import { Link, BrowserRouter as Router } from "react-router-dom";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const HeaderCurio = () => {
   //Used to toggle the hamburger menu
@@ -11,28 +11,45 @@ const HeaderCurio = () => {
 
   return (
     <>
-
       <div className="curio__header">
-          {/* Wrapping the Link components within the Router component */}
-      <Router>
-         {/* Logo linking to the homepage */}
-        <Link to="/" style={{ textDecoration: "none" }}>
-        <div className="gradient__text">
-          <h1>
-            <img src={Curiologo} alt="Curio" className="curio__icon" />
-            <span>URIO</span>
-          </h1>
-          <p className="curio__tag">
-            <b>Your Video Translator :)</b>
-          </p>
-        </div>
-        </Link>
+        {/* Wrapping the Link components within the Router component */}
+        <Router>
+          {/* Logo linking to the homepage */}
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <div className="gradient__text">
+              <h1>
+                <img src={Curiologo} alt="Curio" className="curio__icon" />
+                <span>URIO</span>
+              </h1>
+              <p className="curio__tag">
+                <b>Your Video Translator :)</b>
+              </p>
+            </div>
+          </Link>
 
-        <div className="curio__sign">
+          <div className="curio__sign">
             {/* Social icons */}
-            <FaGithub />
-            <FaLinkedin /> 
-            <FaTwitter />
+            <a
+              href="https://github.com/Curio-org/Curio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/curio-cic"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://twitter.com/_CURI0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter />
+            </a>
           </div>
         </Router>
       </div>
@@ -40,50 +57,55 @@ const HeaderCurio = () => {
      Screen width is < 768px above is disabled and below is triggered
      Added hamburger menu and generic options for the menu
      */}
-      { }
+      {}
       <div className="curio__header-m">
-         {/* Wrapping the Link components within the Router component */}
-         <Router>
+        {/* Wrapping the Link components within the Router component */}
+        <Router>
           {/* Logo linking to the homepage */}
           <Link to="/" style={{ textDecoration: "none" }}>
-        <div className=" gradient__text  gradient__text-m">
-          <h1>
-            <img src={Curiologo} alt="Curio" className="curio__icon" />
-            <span>URIO</span>
-          </h1>
-          <p className="curio__tag">
-            <b>Your Video Translator :)</b>
-          </p>
-        </div>
-        </Link>
+            <div className=" gradient__text  gradient__text-m">
+              <h1>
+                <img src={Curiologo} alt="Curio" className="curio__icon" />
+                <span>URIO</span>
+              </h1>
+              <p className="curio__tag">
+                <b>Your Video Translator :)</b>
+              </p>
+            </div>
+          </Link>
 
-        <button onClick={() => { setTriggerMenu(!triggerMenu) }} className="hamburger-m" type="button">
-          {/*
+          <button
+            onClick={() => {
+              setTriggerMenu(!triggerMenu);
+            }}
+            className="hamburger-m"
+            type="button"
+          >
+            {/*
           This is the hamburger line icon
           First one is when it's off
           Second one is when it's on
           */}
-          <div
-            className={triggerMenu === false ? "hamburger-m" : "display-none"}
-          >
-            <div className="hamburger-lines-m"></div>
-            <div className="hamburger-lines-m"></div>
-            <div className="hamburger-lines-m"></div>
-          </div>
-          <div
-            className={triggerMenu === true ? "hamburger-m" : "display-none"}
-          >
-            <div className="lines-rotated-left-m"></div>
+            <div
+              className={triggerMenu === false ? "hamburger-m" : "display-none"}
+            >
+              <div className="hamburger-lines-m"></div>
+              <div className="hamburger-lines-m"></div>
+              <div className="hamburger-lines-m"></div>
+            </div>
+            <div
+              className={triggerMenu === true ? "hamburger-m" : "display-none"}
+            >
+              <div className="lines-rotated-left-m"></div>
 
-            <div className="lines-rotated-right-m"></div>
-          </div>
-        </button>
-
-      </Router>
+              <div className="lines-rotated-right-m"></div>
+            </div>
+          </button>
+        </Router>
       </div>
       {/*
-    The menu of the hamburger 
-    If trigger is true, display it, if not don't*/}
+        The menu of the hamburger 
+        If trigger is true, display it, if not don't*/}
       <div
         className={
           triggerMenu === true ? "hamburger-menu-active" : "display-none"
